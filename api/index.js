4aeea9d47cb6590a489c34dac010f8b1b38e2fcc
Nico Gonzales <nekulogeon@gmail.com>
2023-05-01 20:54:59 +0800

    commit

--- api/index.js
+++ api/index.js
@@ -8,7 +8,10 @@ app.use(cors());
 
 app.get("/api/test", (req, res) => {
   res.json("hello world " + Date.now());
+  console.log(/api/test)
 });
+
+console.log(app)
 
 if (process.env.API_PORT) {
     app.listen(process.env.API_PORT);

fb2033c220ea10e3746aeeea65da81689273d385
Nico Gonzales <nekulogeon@gmail.com>
2023-05-01 20:18:49 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [result, setResult] = useState('');
+
+  const fetchInfo = async () => {
+    const url = `${import.meta.env.VITE_API_URL}/test`
+    const response = await fetch(url)
+    const json = await response.json()
+    setResult(json)
+  };
+  return (
+    <div>
+      <button onClick={() => fetchInfo()}>fetch info from api</button>
+      <div>result: {result}</div>
+    </div>
+  );
+}
+
+export default App;
